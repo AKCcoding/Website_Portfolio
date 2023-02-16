fea6eade28018512bb60e378df654c049ed7b457
AKCcoding <alraeatwork@gmail.com>
2023-02-16 19:33:21 +0800

    Added Javascript code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Full-Stack Developer","Software Engineer","Cybersecurity Analyst","UI/UX Developer","Database Administrator"],
+    typespeed:100,
+    Backspeed:60,
+    loop:true
+})
