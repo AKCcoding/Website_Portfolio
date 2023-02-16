6a7b7e7f09517af841b9697a307ee3d60ce8fe3a
AKCcoding <alraeatwork@gmail.com>
2023-02-16 18:17:41 +0800

    Adding javascript functions

---
+++ js/style-switcher.js
@@ -0,0 +1,5 @@
+/*Toggle Style Switcher*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector("style-switcher").classList.toggle("open");
+})
